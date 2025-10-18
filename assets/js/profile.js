aed0f50fd47456cd2d8bdc8b70cfadca45da4822
Zoomacs <zoooomax@yahoo.com>
2025-10-19 00:07:30 +0300

    ..

---
+++ assets/js/profile.js
@@ -0,0 +1,23 @@
+// 1. Find the HTML elements and store them in variables
+
+
+         const toggleLink = document.querySelectorAll('.edit-btn');
+
+        // Find all the divs to toggle
+        const allHiddenDivs = document.querySelectorAll('.edit');
+
+        // Add the 'click' event listener
+        // We add 'event' here so we can use it inside
+        toggleLink.addEventListener('click', (event) => {
+            
+            // --- THE JAVASCRIPT CHANGE ---
+            // This stops the <a> tag's default behavior
+            // (which is to navigate to the '#' and jump the page)
+            event.preventDefault(); 
+            // -----------------------------
+
+            // This part is exactly the same as before
+            allHiddenDivs.forEach(div => {
+                div.classList.toggle('edit');
+            });
+            });
