73d3bf034c5d742ab3eeb9ed23a67c3142f6f0b7
Zoomacs <zoooomax@yahoo.com>
2025-10-19 23:54:59 +0300

    some fixes

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,9 +1,14 @@
 const toggleLink = document.querySelector(".edit-btn");
-const allHiddenDivs = document.querySelectorAll(".edit");
+const  select = document.querySelector(".edit-btn");
+const allHiddenDivs = document.querySelectorAll("select");
 
 toggleLink.addEventListener("click", (event) => {
   event.preventDefault();
   allHiddenDivs.forEach((div) => {
     div.classList.toggle("edit");
   });
+});
+select.addEventListener("click", (event) => {
+  event.preventDefault();
+
 });
--- assets/js/themeToggle.js
+++ assets/js/themeToggle.js
@@ -1,29 +1,38 @@
-//Remove localStorage item later , i put it now for testing
 window.onload = () => {
-    if(localStorage.getItem("theme")==="darkmode"){
-        document.body.classList.add("darkmode");
-        const toggle = document.querySelector(".themeToggle")
-        toggle.innerHTML=`
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-icon lucide-moon"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>`
-    }
-}
-
-
-//toggle darkmode
-const themeToggle = () =>{  
-    document.body.classList.toggle("darkmode");
-    
-    document.body.classList.contains("darkmode")?localStorage.setItem("theme","darkmode"):localStorage.setItem("theme","lightmode");
-    
-    const toggle = document.querySelector(".themeToggle")
-    if(document.body.classList.contains("darkmode")){
-        toggle.innerHTML=`
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-icon lucide-moon"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>`
-    }
-    else{
-        toggle.innerHTML=`
+  if (localStorage.getItem("theme") === "darkmode") {
+    document.body.classList.add("darkmode");
+    document.querySelectorAll(".themeToggle").forEach((toggle) => {
+      toggle.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-icon lucide-moon"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>`;
+    });
+  } else {
+    document.querySelectorAll(".themeToggle").forEach((toggle) => {
+      toggle.innerHTML = `
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-icon lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>
-      `
-    }
-}
+      `;
+    });
+  }
+};
 
+function themeToggle() {
+  document.body.classList.toggle("darkmode");
+
+  if (document.body.classList.contains("darkmode")) {
+    localStorage.setItem("theme", "darkmode");
+  } else {
+    localStorage.setItem("theme", "lightmode");
+  }
+
+  if (document.body.classList.contains("darkmode")) {
+    document.querySelectorAll(".themeToggle").forEach((toggle) => {
+      toggle.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-moon-icon lucide-moon"><path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401"/></svg>`;
+    });
+  } else {
+    document.querySelectorAll(".themeToggle").forEach((toggle) => {
+      toggle.innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sun-icon lucide-sun"><circle cx="12" cy="12" r="4"/><path d="M12 2v2"/><path d="M12 20v2"/><path d="m4.93 4.93 1.41 1.41"/><path d="m17.66 17.66 1.41 1.41"/><path d="M2 12h2"/><path d="M20 12h2"/><path d="m6.34 17.66-1.41 1.41"/><path d="m19.07 4.93-1.41 1.41"/></svg>
+      `;
+    });
+  }
+}
