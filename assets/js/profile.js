a0f5d4777ec31ff9e9af2673d6082e74f60f3c06
Zoomacs <zoooomax@yahoo.com>
2025-10-19 19:13:26 +0300

    add profile icon in nav bar

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,19 +1,8 @@
-// 1. Find the HTML elements and store them in variables
-
 const toggleLink = document.querySelector(".edit-btn");
-
 const allHiddenDivs = document.querySelectorAll(".edit");
 
-// Add the 'click' event listener
-// We add 'event' here so we can use it inside
 toggleLink.addEventListener("click", (event) => {
-  // --- THE JAVASCRIPT CHANGE ---
-  // This stops the <a> tag's default behavior
-  // (which is to navigate to the '#' and jump the page)
   event.preventDefault();
-  // -----------------------------
-
-  // This part is exactly the same as before
   allHiddenDivs.forEach((div) => {
     div.classList.toggle("edit");
   });
