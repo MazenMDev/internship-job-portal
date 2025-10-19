cc96976fbe3e287be2ca15d9ce9c0055c6d57020
Zoomacs <zoooomax@yahoo.com>
2025-10-20 00:13:35 +0300

    fixed profile js

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,14 +1,9 @@
 const toggleLink = document.querySelector(".edit-btn");
-const  select = document.querySelector(".edit-btn");
-const allHiddenDivs = document.querySelectorAll("select");
+const allHiddenDivs = document.querySelectorAll(".edit");
 
 toggleLink.addEventListener("click", (event) => {
   event.preventDefault();
   allHiddenDivs.forEach((div) => {
     div.classList.toggle("edit");
   });
-});
-select.addEventListener("click", (event) => {
-  event.preventDefault();
-
 });
