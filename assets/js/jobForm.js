24062728ea5e0de6231de75829a936985c9bef4d
KareemHassan <kareem99710@gmail.com>
2025-10-26 21:45:38 +0300

    refactor: improve layout and styling in job form and profile sections

--- assets/js/jobForm.js
+++ assets/js/jobForm.js
@@ -47,7 +47,6 @@ document.addEventListener("DOMContentLoaded", () => {
     spanInner.classList.add("close-btn-inner");
     spanInner.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg>
-    Back
     `;
 
     closeBtn.classList.add("close-btn");
