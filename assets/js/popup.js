9dcc6e3e518b6d50656ab5acbd20e6d16d037e8d
KareemHassan <kareem99710@gmail.com>
2025-11-15 22:28:43 +0200

    add popup for change password and email

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -25,13 +25,14 @@ document.addEventListener("DOMContentLoaded", () => {
     settingsMenu.classList.toggle("active");
   });
 
-  changeEmailBtn?.addEventListener("click", function () {
-    alert("Change Email clicked");
+  changeEmailBtn.addEventListener("click", function () {
+    createPopUp(true, false);
   });
 
-  forgotPasswordBtn?.addEventListener("click", function () {
-    alert("Forgot Password clicked");
+  forgotPasswordBtn.addEventListener("click", function () {
+      createPopUp(false, true);
   });
+
 
   const companyForm = document.getElementById("form");
   companyForm.addEventListener("submit", async (e) => {
@@ -52,3 +53,56 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function createPopUp(mail = false , password = true){
+  document.body.classList.add("popup-open");
+  if(mail){
+    const popupMail = document.createElement("div");
+    popupMail.classList.add("popup-mail");
+    popupMail.classList.add("popup-change");
+
+    popupMail.innerHTML = `
+      <div class="popup-content">
+        <span class="close-mail-btn">&times;</span>
+        <h2>Change Email</h2>
+        <input type="email" id="newEmail" placeholder="Enter new email" />
+        <input type="password" id="currentPassword" placeholder="Enter current password" />
+        <button id="submitEmailBtn">Submit</button>
+      </div>
+    `;
+    document.body.appendChild(popupMail);
+    document.querySelector(".close-mail-btn").addEventListener("click", function(){
+      document.body.classList.remove("popup-open");
+      document.body.removeChild(popupMail);
+    });
+  }
+  else if(password){
+    const popupPass = document.createElement("div");
+    popupPass.classList.add("popup-pass");
+    popupPass.classList.add("popup-change");
+    popupPass.innerHTML = `
+      <div class="popup-content">
+        <span class="close-pass-btn">&times;</span>
+        <h2>Forgot Password</h2>
+        <input type="password" id="currentPassword" placeholder="Enter current password" />
+        <input type="password" id="newPassword" placeholder="Enter new password" />
+        <input type="password" id="confirmNewPassword" placeholder="Confirm new password" />
+        <button id="submitPassBtn">Submit</button>
+      </div>
+    `;
+    
+    document.body.appendChild(popupPass);
+    document.querySelector(".close-pass-btn").addEventListener("click", function(){
+      document.body.classList.remove("popup-open");
+      document.body.removeChild(popupPass);
+    });
+  }
+
+  document.getElementById("blurred-background").addEventListener("click", function(){
+    document.body.classList.remove("popup-open");
+    const popup = document.querySelector(".popup-change");
+    if(popup){
+      document.body.removeChild(popup);
+    }
+  });
+}
