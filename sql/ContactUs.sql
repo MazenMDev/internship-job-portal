8cfd59e1e43c08e13ca0e588ddd031e8ccd7e1bd
pharoah4ee <eldalymostafa068@gmail.com>
2025-11-17 22:22:08 +0200

    deleted contact us

--- sql/ContactUs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE company (
-    message_id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(150) NOT NULL,
-    message VARCHAR(500),
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    user_Id INT FOREIGN KEY REFERENCES users(Id),
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL
-);
