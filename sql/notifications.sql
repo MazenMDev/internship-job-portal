bc6aeada377b6b1f5deeacdb4d568cc5b9f6abdd
Mostafa Mohamed <mostafa.mohamed.saeed152@gmail.com>
2025-12-10 23:59:41 +0200

    update notification sender type

--- sql/notifications.sql
+++ sql/notifications.sql
@@ -5,7 +5,7 @@ CREATE TABLE notifications (
     description     TEXT,
     form_sender_id  INT NOT NULL,
     user_sender_id  INT NOT NULL,
-    sender_type     ENUM('user', 'company') NOT NULL,
+    sender_type     VARCHAR(20) NOT NULL
     created_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (receiver_id) REFERENCES users(Id),
     FOREIGN KEY (form_sender_id) REFERENCES company(user_id),
