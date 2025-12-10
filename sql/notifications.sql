cb2c7325f6bf34982764e20d7d8d5765ecbed1c3
Mostafa Mohamed <mostafa.mohamed.saeed152@gmail.com>
2025-12-10 23:53:46 +0200

    notifications sql

---
+++ sql/notifications.sql
@@ -0,0 +1,13 @@
+CREATE TABLE notifications (
+    notif_id        INT AUTO_INCREMENT PRIMARY KEY,
+    receiver_id     INT NOT NULL,
+    title           VARCHAR(255) NOT NULL,
+    description     TEXT,
+    form_sender_id  INT NOT NULL,
+    user_sender_id  INT NOT NULL,
+    sender_type     ENUM('user', 'company') NOT NULL,
+    created_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (receiver_id) REFERENCES user(Id),
+    FOREIGN KEY (form_sender_id) REFERENCES company(user_id),
+    FOREIGN KEY (user_sender_id) REFERENCES user(Id)
+);
