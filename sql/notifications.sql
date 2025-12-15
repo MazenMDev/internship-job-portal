cae2c8fad16349b6d39542a8e2a74baa2469fc7f
Mohanadyasser-eng <mohandyahmed@gmail.com>
2025-12-15 19:59:47 +0200

    notification php update

--- sql/notifications.sql
+++ sql/notifications.sql
@@ -3,11 +3,8 @@ CREATE TABLE notifications (
     receiver_id     INT NOT NULL,
     title           VARCHAR(255) NOT NULL,
     description     TEXT,
-    form_sender_id  INT NOT NULL,
-    user_sender_id  INT NOT NULL,
+    sender_id  INT NOT NULL,
     sender_type     VARCHAR(20) NOT NULL,
+    receiver_type    VARCHAR(20) NOT NULL,
     created_at      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (receiver_id) REFERENCES users(Id),
-    FOREIGN KEY (form_sender_id) REFERENCES company(company_id),
-    FOREIGN KEY (user_sender_id) REFERENCES users(Id)
 );
