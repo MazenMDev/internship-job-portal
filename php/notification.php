5ceefa803f0c4b052ef7b5622fb0e007c59c2eff
KareemHassan <kareem99710@gmail.com>
2025-12-20 20:54:25 +0200

    fix notification seen update and images

--- php/notification.php
+++ php/notification.php
@@ -17,11 +17,18 @@ else {
     $current_receiver_type = 2;
 }
 
+$stmt = $conn->prepare("UPDATE notifications 
+                        SET seen = 1 
+                        WHERE receiver_id = ? 
+                        AND receiver_type = ?");
+
+$stmt->bind_param("ii", $current_user_id, $current_receiver_type);
+$stmt->execute();
 
 $stmt = $conn->prepare("SELECT * FROM notifications 
                         WHERE receiver_id = ? 
                         AND receiver_type = ?
-                        ORDER BY created_at");
+                        ORDER BY created_at DESC");
 
 
 $stmt->bind_param("ii", $current_user_id, $current_receiver_type);
@@ -30,8 +37,49 @@ $stmt->execute();
 $result = $stmt->get_result();
 
 $notifications = [];
+
 while ($row = $result->fetch_assoc()) {
     $notifications[] = $row;
+    $sender_type = $row['sender_type'];
+    $sender_id = $row['sender_id'];
+    // 1 for user , 2 for company , 3 for job
+    $sender_type = intval($sender_type);
+    if ($sender_type === 1) {
+        $stmt_sender = $conn->prepare("SELECT Image FROM users WHERE user_id = ?");
+        $stmt_sender->bind_param("i", $sender_id);
+        $res = $stmt_sender->execute();
+        $result_sender = $stmt_sender->get_result();
+        if($result_sender->num_rows > 0){
+            $result_sender = $result_sender->fetch_assoc();
+            $image = $result_sender['Image'];
+            $path = null;
+            if($image == 'profile.jpeg'){
+                $path = '/ImageStorage/profile.jpeg';
+            }
+            else{
+                $path = '/ImageStorage/users/'.$sender_id.'/'.$image;
+            }
+            $notifications[count($notifications)-1]['image'] = $path;
+        }
+        
+    } else{
+        $stmt_sender = $conn->prepare("SELECT Image FROM company WHERE company_id = ?");
+        $stmt_sender->bind_param("i", $sender_id);
+        $res = $stmt_sender->execute();
+        $result_sender = $stmt_sender->get_result();
+        if($result_sender->num_rows > 0){
+            $result_sender = $result_sender->fetch_assoc();
+            $img = $result_sender['Image'];
+            $path = null;
+            if($img == 'company.png'){
+                $path = '/ImageStorage/company.png';
+            }
+            else{
+                $path = '/ImageStorage/companies/'.$sender_id.'/'.$img;
+            }
+            $notifications[count($notifications)-1]['image'] = $path;
+        }
+    }
 }
 
 header('Content-Type: application/json');
